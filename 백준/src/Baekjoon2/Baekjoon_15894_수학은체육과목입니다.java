47a30aea19188bf5e0dbc057dfeea38e681dbd1a
KCY <ghkdua059@gmail.com>
2020-05-18 23:20:58 +0900

    수학은체육과목입니다 bronze5

---
+++ 백준/src/Baekjoon2/Baekjoon_15894_수학은체육과목입니다.java
@@ -0,0 +1,12 @@
+package Baekjoon2;
+
+import java.util.Scanner;
+
+public class Baekjoon_15894_수학은체육과목입니다 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextLong()*4);
+	}
+
+}
