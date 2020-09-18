7e9c80aab47107c6c9bf2b53d4e8c65a5794bf7a
KCY <ghkdua059@gmail.com>
2020-09-18 09:36:03 +0900

    삽입 정렬 구현

---
+++ 기술연습/src/sort/InsertSort.java
@@ -0,0 +1,27 @@
+package sort;
+
+import java.util.Arrays;
+
+public class InsertSort {
+	// 삽입 정렬
+	public static void main(String[] args) {
+		int[] arr = { 5, 1, 6, 7, 8, 75, 5, 65, 456, 353, 45, 45, 3425, 43, 3, 2, 4 };
+		insertSort(arr);
+	}
+
+	static void insertSort(int[] arr) {
+		for (int i = 1; i < arr.length; i++) {
+			int temp = i;
+			for (int j = i - 1; j >= 0; j--) {
+				if (arr[temp] < arr[j]) {
+					int t = arr[temp];
+					arr[temp] = arr[j];
+					arr[j] = t;
+					temp--;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
