f9452549e33b6a0e5579e353444c8d9a52a81b74
KCY <ghkdua059@gmail.com>
2021-12-28 22:56:29 +0900

    DP RGB2

--- 백준/src/baekjoon5/Baekjoon_12865_평범한배낭.java
+++ 백준/src/baekjoon5/Baekjoon_12865_평범한배낭.java
@@ -3,7 +3,6 @@ package baekjoon5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Baekjoon_12865_평범한배낭 {
---
+++ 백준/src/baekjoon5/Baekjoon_17404_RGB거리2.java
@@ -0,0 +1,46 @@
+package baekjoon5;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_17404_RGB거리2 {
+	static int INF = 12345678;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[][] board = new int[N][3];
+		for (int r = 0; r < N; r++) {
+			StringTokenizer token = new StringTokenizer(br.readLine());
+			for (int c = 0; c < 3; c++) {
+				board[r][c] = Integer.parseInt(token.nextToken());
+			}
+		}
+		int result = INF;
+		for (int t = 0; t < 3; t++) {
+			int[][] copy = new int[N][3];
+			for (int r = 0; r < N; r++) {
+				for (int c = 0; c < 3; c++) {
+					copy[r][c] = board[r][c];
+				}
+			}
+			for (int i = 0; i < 3; i++) {
+				if (i == t) {
+					copy[N - 1][i] = INF;
+				} else {
+					copy[0][i] = INF;
+				}
+			}
+			for (int r = 1; r < N; r++) {
+				copy[r][0] += Math.min(copy[r - 1][1], copy[r - 1][2]);
+				copy[r][1] += Math.min(copy[r - 1][0], copy[r - 1][2]);
+				copy[r][2] += Math.min(copy[r - 1][0], copy[r - 1][1]);
+			}
+			result = Math.min(result, Math.min(copy[N - 1][0], Math.min(copy[N - 1][1], copy[N - 1][2])));
+		}
+		System.out.println(result);
+	}
+
+}
